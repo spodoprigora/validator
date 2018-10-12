7f2e293427ba318d3eb5ed45cf66792bf96c4888
r.abelharisov <r.abelharisov@andersenlab.com>
2018-10-12 16:09:58 +0300

    validator update

--- index.php
+++ index.php
@@ -8,8 +8,9 @@ class Validator {
      */
     public function validate(string $id): bool
     {
-        if (preg_match("/^(\d{2})(\d{2})(\d{3})(\d{2})(\d{3})(\d{1})$/", $id, $match) === 0)
+        if (preg_match("/^(\d{2})(\d{2})(\d{3})(\d{2})(\d{3})(\d{1})$/", $id, $match) === 0) {
             return false;
+        }
 
         $day = $match[1];
         $month = $match[2];
@@ -18,18 +19,17 @@ class Validator {
         $random = $match[5];
         $k = $match[6];
 
-        if (!(1 <= $day && $day <= 31)
-            || !(1 <= $month && $month <= 12)
-            || !(10 <= $region && $region <= 19))
-            return false;
+        $year = $year[0] === 0 ? 2 . $year : 1 . $year;
 
         $checkSum = 11-((7*($day[0] + $year[2]) + 6*($day[1] + $region[0]) + 5*($month[0] + $region[1]) + 4*($month[1] + $random[0]) + 3*($year[0] + $random[1]) + 2*($year[1] + $random[2])));
 
         $calcK = $this->mod($checkSum, 11);
 
-        if($calcK === 10 || $calcK === 11 && $k !== 0)
-            return false;
-        return true;
+        $result = true;
+        if(($calcK === 10 || $calcK === 11) && $k === 0) {
+            $result = false;
+        }
+        return checkdate($month, $day, $year) && ($region >= 10 && $region <= 19) && $result;
     }
 
     /**
@@ -40,9 +40,11 @@ class Validator {
     protected function mod(int $n, int $d) :int
     {
         $res = $n % $d;
-        if ($n < 0)
+        if ($n < 0) {
             $res += $d;
+        }
         return $res;
     }
 
 }
+
