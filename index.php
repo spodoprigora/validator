0f94b31c4db291908a5fea9d80562dff611568b2
r.abelharisov <r.abelharisov@andersenlab.com>
2018-10-12 13:34:44 +0300

    validator update

--- index.php
+++ index.php
@@ -8,26 +8,28 @@ class Validator {
      */
     public function validate(string $id): bool
     {
-        if (strlen($id) != 13 || !is_numeric($id))
+        if (preg_match("/^(\d{2})(\d{2})(\d{3})(\d{2})(\d{3})(\d{1})$/", $id, $match) === 0)
             return false;
 
-        //region
-        if ($id[7] !== '1')
+        $day = $match[1];
+        $month = $match[2];
+        $year = $match[3];
+        $region = $match[4];
+        $random = $match[5];
+        $k = $match[6];
+
+        if (!(1 <= $day && $day <= 31)
+            || !(1 <= $month && $month <= 12)
+            || !(10 <= $region && $region <= 19))
             return false;
 
-        $dd = (int)($id[0] . $id[1]);
-        $mm = (int)($id[2] .$id[3]);
-        if (!(1 <= $dd && $dd <= 31) || !(1 <= $mm && $mm <= 12))
-            return false;
-
-        $checkSum = 11-((7*($id[0] + $id[6]) + 6*($id[1] + $id[7]) + 5*($id[2] + $id[8]) + 4*($id[3] + $id[9]) + 3*($id[4] + $id[10]) + 2*($id[5] + $id[11])));
+        $checkSum = 11-((7*($day[0] + $year[2]) + 6*($day[1] + $region[0]) + 5*($month[0] + $region[1]) + 4*($month[1] + $random[0]) + 3*($year[0] + $random[1]) + 2*($year[1] + $random[2])));
 
         $calcK = $this->mod($checkSum, 11);
 
-        if($calcK === 10 || $calcK === 11 && $id[12] !== 0)
+        if($calcK === 10 || $calcK === 11 && $k !== 0)
             return false;
         return true;
-
     }
 
     /**
@@ -35,17 +37,12 @@ class Validator {
      * @param int $d
      * @return int
      */
-    function mod(int $n, int $d) :int
+    protected function mod(int $n, int $d) :int
     {
-        return $n - $d * floor($n/$d);
+        $res = $n % $d;
+        if ($n < 0)
+            $res += $d;
+        return $res;
     }
 
 }
-$obj = new Validator();
-echo $obj->validate('aaa');
-echo $obj->validate('1aaaaaaaaaaaaa');
-echo $obj->validate('1abcde1eeeeee');
-echo $obj->validate('1407976101522');
-echo $obj->validate('1407976101522');
-
-
